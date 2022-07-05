b572287ec621ed41e2e5bea2e2ef458c8e9d1b20
Uniplac <github@uniplaclages.edu.br>
2022-07-05 10:24:29 -0300

    afd

--- pages/perfil_autor.js
+++ pages/perfil_autor.js
@@ -1,14 +1,12 @@
 import Post from "./components/Post";
-import { FaFilter } from 'react-icons/fa';
+import { FaFilter } from "react-icons/fa";
 
 export default function Noticias() {
   return (
     <div className="min-h-screen pt-14 mb-16 drop-shadow-xl">
-
-<div >
-      <h1 className="titulo-principal">Perfil Autor</h1>
+      <div>
+        <h1 className="titulo-principal">Perfil Autor</h1>
       </div>
-
     </div>
   );
 }
