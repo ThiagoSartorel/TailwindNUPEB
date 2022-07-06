53ee60f03df0bec58ee1a18205aee5b1cd75ee4d
Uniplac <github@uniplaclages.edu.br>
2022-07-06 10:07:30 -0300

    EDQD

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -7,108 +7,90 @@ import Contact from "./contact";
 export default function Home() {
   return (
     <>
-
-
       <div className="sm:mx-20 md:mx-30 lg:mx-48 min-h-screen mb-8 pt-12">
-      <div className="texto-basico">
+        <div className="texto-basico">
+          <h2 className="titulo-principal">Linhas de Pesquisa</h2>
+          <br></br>
+          <h3>Pedagogia da Infância, Políticas e Diferenças</h3>
+          <u>Objetivo:</u>
+          <p>
+            Investigar temas ligados à infância e às políticas educacionais,
+            numa perspectiva pós-estruturalista, buscando ampliar o conhecimento
+            sobre as metodologias de pesquisa com crianças, pesquisas com
+            documentos, análise foucaultiana do discurso, estudos culturais
+            sobre educação, escola, diferença e diversidade.
+          </p>
+          <u>Coordenador(a): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Pesquisadoras(os): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Mestrandas(os): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Egressas(os) da Graduação: </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Egressas(os) do Ensino Médio: </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <br></br>
 
-      <h2 className="titulo-principal">Linhas de Pesquisa</h2>
-      <br></br>
-      <h3>Pedagogia da Infância, Políticas e Diferenças</h3>
-      <u>Objetivo:</u>
-      <p>
-        Investigar temas ligados à infância e às políticas educacionais,
-        numa perspectiva pós-estruturalista, buscando ampliar o
-        conhecimento sobre as metodologias de pesquisa com crianças,
-        pesquisas com documentos, análise foucaultiana do discurso,
-        estudos culturais sobre educação, escola, diferença e diversidade.
-      </p>
-      <u>Coordenador(a): </u>
-      <a href="" target="_blank" >
-      
-      </a><br></br>
-      <u>Pesquisadoras(os): </u>
-      <a href="" target="_blank" >
-      
-      </a><br></br>
-      <u>Mestrandas(os): </u>
-      <a href="" target="_blank" >
+          <h3>Comunicação, Divulgação Científica e Inovação</h3>
+          <u>Objetivo:</u>
+          <p>
+            Desenvolver pesquisas básicas e aplicadas na área da comunicação,
+            divulgação científica e inovação.
+          </p>
+          <u>Coordenador(a): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Pesquisadoras(os): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Mestrandas(os): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Egressas(os) da Graduação: </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Egressas(os) do Ensino Médio: </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <br></br>
 
-      </a><br></br>
-      <u>Egressas(os) da Graduação: </u>
-      <a href="" target="_blank" >
-
-      </a><br></br>
-      <u>Egressas(os) do Ensino Médio: </u>
-      <a href="" target="_blank" >
-    
-      </a>
-      <br></br><br></br>
-
-      <h3>Comunicação, Divulgação Científica e Inovação</h3>
-      <u>Objetivo:</u>
-      <p>
-        Desenvolver pesquisas básicas e aplicadas na área da comunicação,
-        divulgação científica e inovação.
-      </p>
-      <u>Coordenador(a): </u>
-      <a href="" target="_blank" >
-      
-      </a><br></br>
-      <u>Pesquisadoras(os): </u>
-      <a href="" target="_blank" >
-      
-      </a><br></br>
-      <u>Mestrandas(os): </u>
-      <a href="" target="_blank" >
-
-      </a><br></br>
-      <u>Egressas(os) da Graduação: </u>
-      <a href="" target="_blank" >
-
-      </a><br></br>
-      <u>Egressas(os) do Ensino Médio: </u>
-      <a href="" target="_blank" >
-    
-      </a>
-      <br></br><br></br>
-
-      <h3>Formação de professores e Cotidiano escolar</h3>
-      <u>Objetivo:</u>
-      <p>
-        Investigar os processos formativos no ambiente educacional,
-        abrangendo os diferentes focos das políticas para a Educação
-        Básica como formação inicial e continuada de professores,
-        avaliação, currículo, direitos humanos e diversidade, gestão
-        democrática e financiamento.
-      </p>
-      <u>Coordenador(a): </u>
-      <a href="" target="_blank" >
-      
-      </a><br></br>
-      <u>Pesquisadoras(os): </u>
-      <a href="" target="_blank" >
-      
-      </a><br></br>
-      <u>Mestrandas(os): </u>
-      <a href="" target="_blank" >
-
-      </a><br></br>
-      <u>Egressas(os) da Graduação: </u>
-      <a href="" target="_blank" >
-
-      </a><br></br>
-      <u>Egressas(os) do Ensino Médio: </u>
-      <a href="" target="_blank" >
-    
-      </a>
-      <br></br><br></br>
-    </div>
-          </div>
-     
+          <h3>Formação de professores e Cotidiano escolar</h3>
+          <u>Objetivo:</u>
+          <p>
+            Investigar os processos formativos no ambiente educacional,
+            abrangendo os diferentes focos das políticas para a Educação Básica
+            como formação inicial e continuada de professores, avaliação,
+            currículo, direitos humanos e diversidade, gestão democrática e
+            financiamento.
+          </p>
+          <u>Coordenador(a): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Pesquisadoras(os): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Mestrandas(os): </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Egressas(os) da Graduação: </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <u>Egressas(os) do Ensino Médio: </u>
+          <a href="" target="_blank"></a>
+          <br></br>
+          <br></br>
+        </div>
+        <h2 className="titulo-principal">Pesquisas em andamento</h2>
         
-     
+        <h2 className="titulo-principal">Pesquisas concluídas</h2>
+
+      </div>
     </>
   );
 }
-
