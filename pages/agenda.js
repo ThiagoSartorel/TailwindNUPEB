8bec8aa27790b4e855bb704857413afa96c56fa2
Uniplac <github@uniplaclages.edu.br>
2022-07-07 09:45:05 -0300

    agenda

--- pages/agenda.js
+++ pages/agenda.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { BsSearch } from "react-icons/bs";
 import { FaSearch } from "react-icons/fa";
 
 export default function Calendario() {
@@ -9,19 +10,29 @@ export default function Calendario() {
         <div className="shadow-lg">
           <div className="md:py-8 py-5 md:px-16 px-5 dark:bg-gray-700 bg-gray-100 rounded-b w-full rounded-lg">
             <div className="flex flex-row">
-              <div className="flex-none mt-1">
-                <label className="px-4 pr-4">Buscar</label>
-              </div>
+             
               <div>
                 <input className="mb-4 px-2 py-1 w-auto" type="month"></input>
               </div>
+              
               <div className="w-full flex flex-row-reverse">
-                <FaSearch className="my-2 mx-4 hover:text-[#f7a439] hover:scale-110" />
+                <FaSearch className="my-4 mx-3 hover:text-[#f7a439] hover:scale-110" />
               </div>
+              <div className="bg-gray-200 text-white flex justify-between items-center max-w-[200px] w-full m-auto p-2 rounded-2xl pl-4">
+              <div>
+                
+                <input
+                  type="text"
+                  className="bg-transparent border-none text-white focus:outline-none"
+                  placeholder="Pesquisar"
+                />
+              </div>
+            </div>
+              
             </div>
             <hr className="mb-4"></hr>
             <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 px-4 gap-4">
-              <div className="bg-gray-100 p4 rounded-lg shadow-md overflow-hidden">
+              <div className="bg-gray-100 rounded-lg shadow-md overflow-hidden">
                 <div className="w-full bg-gray-100 p-2 border-b border-gray-100">
                   <h4 className="font-semibold text-lg">Titulo do evento</h4>
                   <a href="/perfil_autor" className="font-thin">Autor</a>
