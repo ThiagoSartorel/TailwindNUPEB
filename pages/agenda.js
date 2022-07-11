3b8d5e92de97d5883ac3ecd9760db8e8473145bb
Uniplac <github@uniplaclages.edu.br>
2022-07-11 16:08:56 -0300

    agenda

--- pages/agenda.js
+++ pages/agenda.js
@@ -4,38 +4,37 @@ import { FaSearch } from "react-icons/fa";
 
 export default function Calendario() {
   return (
-    <div id="contact" className="w-full min-h-screen ">
-      <div className="sm:mx-20 md:mx-30 lg:mx-48 min-h-screen mb-12 pt-12">
+    <div id="contact" className="">
+      <div className="mb-20 pt-12 mx-4 sm:mx-12 md:mx-16 lg:mx-20 xl:mx-28 min-h-screen">
         <p className="titulo-principal">Agenda</p>
-        <div className="shadow-lg">
-          <div className="md:py-8 py-5 md:px-16 px-5 dark:bg-gray-700 bg-gray-100 rounded-b w-full">
-            <div className="flex flex-row">
-             
+        <div className="shadow-md">
+          <div className=" rounded-lg px-2 py-8 md:py-8 md:px-16 dark:bg-gray-700 bg-gray-100 rounded-b w-full">
+            <div className="flex flex-col sm:justify-between sm:flex-row">
               <div>
                 <input className="mb-4 px-2 py-1 w-auto" type="month"></input>
               </div>
-              
-              <div className="w-full flex flex-row-reverse">
-                <FaSearch className="my-4 mx-3 hover:text-[#f7a439] hover:scale-110" />
+
+              <div className="flex flex-initial ">
+                <div className="bg-gray-200 text-white flex justify-between items-center w-full m-auto p-2 rounded-2xl px-4 mx-6 mb-4">
+                  <div>
+                    <input
+                      type="text"
+                      className="bg-transparent border-none text-white focus:outline-none"
+                      placeholder="Pesquisar"
+                    />
+                  </div>
+                  <FaSearch className=" text-slate-400 hover:text-slate-600 hover:scale-110" />
+                </div>
               </div>
-              <div className="bg-gray-200 text-white flex justify-between items-center max-w-[200px] w-full m-auto p-2 rounded-2xl pl-4">
-              <div>
-                
-                <input
-                  type="text"
-                  className="bg-transparent border-none text-white focus:outline-none"
-                  placeholder="Pesquisar"
-                />
-              </div>
-            </div>
-              
             </div>
             <hr className="mb-4"></hr>
             <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 px-4 gap-4">
               <div className="bg-gray-100 rounded-lg shadow-md overflow-hidden">
                 <div className="w-full bg-gray-100 p-2 border-b border-gray-100">
                   <h4 className="font-semibold text-lg">Titulo do evento</h4>
-                  <a href="/perfil_autor" className="font-thin">Autor</a>
+                  <a href="/perfil_autor" className="font-thin">
+                    Autor
+                  </a>
                 </div>
                 <div>
                   <img
@@ -44,10 +43,11 @@ export default function Calendario() {
                   ></img>
                 </div>
                 <div className="p-4 bg-slate-200">Conteudo</div>
-                <div className="w-full bg-gray-100 p-2 border-b border-gray-100 pb-3">
+                <div className=" bg-gray-100 p-2 border-b border-gray-100 pb-3">
                   <a
                     href="/noticia_evento"
-                    className="bg-green-500 p-1 px-2 rounded-md text-white hover:bg-green-600 absolute">
+                    className="bg-green-500 p-1 px-2 rounded-md text-white hover:bg-green-600 absolute"
+                  >
                     Saiba mais
                   </a>
                   <p className="text-gray-500 text-end mt-2 text-sm">
@@ -57,9 +57,11 @@ export default function Calendario() {
               </div>
 
               <div className="bg-gray-100 p4 rounded-lg shadow-md overflow-hidden">
-                <div className="w-full bg-gray-100 p-2 border-b border-gray-100">
+                <div className=" bg-gray-100 p-2 border-b border-gray-100">
                   <h4 className="font-semibold text-lg">Titulo do evento</h4>
-                  <a href="/perfil_autor" className="font-thin">Autor</a>
+                  <a href="/perfil_autor" className="font-thin">
+                    Autor
+                  </a>
                 </div>
                 <div>
                   <img
@@ -68,10 +70,11 @@ export default function Calendario() {
                   ></img>
                 </div>
                 <div className="p-4 bg-slate-200">Conteudo</div>
-                <div className="w-full bg-gray-100 p-2 border-b border-gray-100 pb-3">
+                <div className=" bg-gray-100 p-2 border-b border-gray-100 pb-3">
                   <a
                     href="/noticia_evento"
-                    className="bg-green-500 p-1 px-2 rounded-md text-white hover:bg-green-600 absolute">
+                    className="bg-green-500 p-1 px-2 rounded-md text-white hover:bg-green-600 absolute"
+                  >
                     Saiba mais
                   </a>
                   <p className="text-gray-500 text-end mt-2 text-sm">
@@ -81,9 +84,11 @@ export default function Calendario() {
               </div>
 
               <div className="bg-gray-100 p4 rounded-lg shadow-md overflow-hidden">
-                <div className="w-full bg-gray-100 p-2 border-b border-gray-00">
+                <div className=" bg-gray-100 p-2 border-b border-gray-00">
                   <h4 className="font-semibold text-lg">Titulo do evento</h4>
-                  <a href="/perfil_autor" className="font-thin">Autor</a>
+                  <a href="/perfil_autor" className="font-thin">
+                    Autor
+                  </a>
                 </div>
                 <div>
                   <img
@@ -92,10 +97,11 @@ export default function Calendario() {
                   ></img>
                 </div>
                 <div className="p-4 bg-slate-200">Conteudo</div>
-                <div className="w-full bg-gray-100 p-2 border-b border-gray-100 pb-3">
+                <div className=" bg-gray-100 p-2 border-b border-gray-100 pb-3">
                   <a
                     href="/noticia_evento"
-                    className="bg-green-500 p-1 px-2 rounded-md text-white hover:bg-green-600 absolute">
+                    className="bg-green-500 p-1 px-2 rounded-md text-white hover:bg-green-600 absolute"
+                  >
                     Saiba mais
                   </a>
                   <p className="text-gray-500 text-end mt-2 text-sm">
