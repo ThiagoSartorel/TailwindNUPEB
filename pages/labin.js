1c8ef25698eaff879057d659b860b05cc7127600
Uniplac <github@uniplaclages.edu.br>
2022-07-12 14:26:52 -0300

    labin

--- pages/labin.js
+++ pages/labin.js
@@ -18,29 +18,23 @@ export default function Home() {
         <div className="texto-basico">
           <h2 className="titulo-principal">LABIN</h2>
 
-          <div className="text-center p-6 bg-slate-500 bg-opacity-10 rounded-2xl shadow-lg">
-            <a className="text-[3rem] text-center font-thin text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439]">
+          <div className="text-center p-8 bg-slate-500 bg-opacity-10 rounded-2xl shadow-lg">
+            <h1 className="pt-4 text-[3rem] text-center font-normal text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439] ">
               Laboratório de Inovação Educacional
-            </a>
+            </h1>
 
-            <div className="text-clip">
-              <svg className="icon">
-                <SpeakerphoneIcon />
-              </svg>
-              <h1 className="pb-3 titulo-text-[3rem] text-center font-thin text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439]">
+            <div className="text-start p-6 ">
+              <h2 className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  flex pb-3 font-normal text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439] text-start shadow-sm">
+                <svg className="icon">
+                  <ShareIcon />
+                </svg>
                 Resumo
-              </h1>
-              <p className="text-justify">
-                LA pesquisa tem com finalidade pesquisar a questão da inovação
-                na educação relacionada à produção de conteúdos e à divulgação
-                científica, promovendo a investigação e a realização de práticas
-                baseadas nos dados pesquisados. O período previsto para sua
-                realização é de dois anos, a partir do segundo semestre de 2021.
-                O projeto tem como objetivo analisar as contribuições da
-                pesquisa científica e das práticas de inovação e divulgação
-                científica realizadas no Laboratório de Produção de Conteúdo e
-                Inovação Educacional (Labin) para a melhoria da qualidade da
-                educação. Busca fortalecer a articulação do grupo de pesquisa
+              </h2>
+              <p className="text-justify p-4">
+                Pesquisar a questão da inovação na educação relacionada à
+                produção de conteúdos e à divulgação científica, promovendo a
+                investigação e a realização de práticas baseadas nos dados
+                pesquisados. Busca fortalecer a articulação do grupo de pesquisa
                 NuPEB com a Educação Básica. Nesse sentido, compreende-se que é
                 fundamental pesquisar e aplicar os conhecimentos presentes na
                 literatura científica no desenvolvimento de conteúdos
@@ -57,10 +51,7 @@ export default function Home() {
                 aperfeiçoamento técnico-científico do Podcast Eureka! servirá
                 como fundamentação para a elaboração de materiais didáticos que
                 estimulem e possibilitem o uso deste recurso nos diferentes
-                níveis de educação. A pesquisa também irá subsidiar a elaboração
-                de dois recursos de divulgação de pesquisas realizadas no NuPEB
-                e na Instituição: o website do Laboratório e o Repositório
-                Digital Institucional. Além disso, o Laboratório aplicará os
+                níveis de educação. Além disso, o Laboratório aplicará os
                 conhecimentos produzidos na produção de conteúdos sobre questões
                 de educação ambiental e de relações étnico-raciais pesquisadas
                 pelo Núcleo de Educação Ambiental e pelo Núcleo de Estudos
@@ -82,27 +73,28 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="text-clip">
-              <svg className="icon">
-                <ShareIcon />
-              </svg>
-              <h1 className="pb-3 text-[3rem] text-center font-thin text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439]">
+            <div className="text-start p-6">
+              <h2 className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex pb-3 font-normal text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439] text-start shadow-sm">
+                <svg className="icon">
+                  <SpeakerphoneIcon />
+                </svg>
                 Finalidade
-              </h1>
-              <p className="text-justify">
+              </h2>
+
+              <p className="text-justify p-4">
                 <ul>
-                  1-Pesquisar a questão da inovação relacionada à produção de
+                  1- Pesquisar a questão da inovação relacionada à produção de
                   conteúdos e à divulgação científica, promovendo a investigação
                   e a realização de práticas baseadas nos dados pesquisados.
                 </ul>
                 <ul>
-                  2-O Labin busca potencializar as atividades de pesquisa
+                  2- O Labin busca potencializar as atividades de pesquisa
                   realizadas no Grupo de Pesquisa NuPEB e sua articulação com a
                   comunidade científica por meio da pesquisa sobre inovação e
                   divulgação científica.
                 </ul>
                 <ul>
-                  3-As atividades de pesquisa do projeto visam estimular
+                  3- As atividades de pesquisa do projeto visam estimular
                   parcerias entre os pesquisadores que atuam na área
                   educacional, buscando investigar e aplicar os conhecimentos na
                   produção de conteúdos inovadores relacionados à divulgação
@@ -113,70 +105,56 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="text-clip">
-              <svg className="icon">
-                <CheckCircleIcon />
-              </svg>
-              <h1 className="pb-3 text-[3rem] text-center font-thin text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439]">
-                Objetivo Geral
-              </h1>
-              <p className="text-justify">
-                Analisar as contribuições da pesquisa científica e das práticas
-                de inovação e divulgação científica realizadas no Laboratório de
-                Produção de Conteúdo e Inovação Educacional (Labin) para a
-                melhoria da qualidade da educação.
-              </p>
-            </div>
+            <div className="text-start p-6">
+              <h2 className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex pb-3 font-normal text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439] text-start shadow-sm">
+                <svg className="icon">
+                  <CheckCircleIcon />
+                </svg>
+                Objetivos
+              </h2>
+              <p className="text-justify p-4">
+                <h3 className="font-medium pb-2">Geral: </h3>
+                <ul>
+                  Analisar as contribuições da pesquisa científica e das
+                  práticas de inovação e divulgação científica realizadas no
+                  Laboratório de Produção de Conteúdo e Inovação Educacional
+                  (Labin) para a melhoria da qualidade da educação.
+                </ul>
 
-            <div className="text-clip">
-              <svg className="icon">
-                <ClipboardCheckIcon />
-              </svg>
-              <h1 className="pb-3 text-[3rem] text-center font-thin text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439]">
-                Objetivos Específicos
-              </h1>
-              <p className="text-justify">
+                <h2 className="font-medium pb-2 pt-2">Específicos:</h2>
                 <ul>
-                  * Analisar as contribuições da pesquisa e das práticas
-                  realizadas no projeto para a melhoria da qualidade da educação
-                  e publicar livro com os resultados desta e de outras pesquisas
-                  educacionais.
+                  1- Pesquisar a questão da inovação relacionada à produção de
+                  conteúdos e à divulgação científica, promovendo a investigação
+                  e a realização de práticas baseadas nos dados pesquisados.
                 </ul>
                 <ul>
-                  * Aperfeiçoar o Podcast Eureka!, nos seus aspectos
-                  técnico-científicos.
+                  2- O Labin busca potencializar as atividades de pesquisa
+                  realizadas no Grupo de Pesquisa NuPEB e sua articulação com a
+                  comunidade científica por meio da pesquisa sobre inovação e
+                  divulgação científica.
                 </ul>
                 <ul>
-                  * Aplicar os conhecimentos coletados na produção de conteúdos
-                  educacionais inovadores e práticas de divulgação científica.
-                </ul>
-                <ul>
-                  * Articular as pesquisas do Labin com as atividades do Núcleo
-                  de Educação Ambiental da Universidade e do Núcleo de Estudos
-                  Afro-Brasileiros da Universidade (NEAB/UNIPLAC).
-                </ul>
-                <ul>
-                  * Desenvolver material didático denominado Dicionário Ciência
-                  na Escola, contendo verbetes didáticos sobre conceitos
-                  relacionados aos currículos escolares e às práticas docentes
-                  na Educação Básica e no Ensino Superior.
-                </ul>
-                <ul>
-                  * Produzir materiais didáticos sobre a produção de podcast, a
-                  partir da experiência do Eureka!
+                  3- As atividades de pesquisa do projeto visam estimular
+                  parcerias entre os pesquisadores que atuam na área
+                  educacional, buscando investigar e aplicar os conhecimentos na
+                  produção de conteúdos inovadores relacionados à divulgação
+                  científica, ampliando a pertinência social e a aplicação dos
+                  resultados das pesquisas do NuPEB na realidade local, regional
+                  e nacional, articulando a pesquisa, o ensino e a extensão.
                 </ul>
               </p>
             </div>
 
-            <div className="text-clip">
-              <svg className="icon">
-                <AtSymbolIcon />
-              </svg>
-              <h1 className="pb-3 text-[3rem] text-center font-thin text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439]">
-                Contato
+            <div className="text-start p-6">
+              <h1 className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex pb-3 font-normal text-xl sm:text-2xl md:text-3xl lg:text-3xl text-[#f7a439] text-start shadow-sm">
+                <svg className="icon">
+                  <AtSymbolIcon />
+                </svg>
+                Coordenação
               </h1>
-              <p className="text-justify">
-                <ul>Coordenação: Prof. Dr. Jaime Farias Dresch</ul>
+
+              <p className="text-justify p-4">
+                <ul>Prof. Dr. Jaime Farias Dresch</ul>
                 <ul>Email: labin@uniplaclages.edu.br</ul>
               </p>
             </div>
