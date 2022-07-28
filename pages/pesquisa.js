c9e99eac6d1b49921f58e2de3a605e04cad89f5b
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-07-28 08:51:03 -0300

    pesquisa dinamico

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,5 +1,8 @@
+import HtmlParser  from "react-html-parser";
+import axios from "axios";
 
-export default function Home() {
+export default function Home(props) {
+
   return (
     <>
       <div className="sm:mx-20 md:mx-30 lg:mx-48 min-h-screen mb-12 pt-12 px-3">
@@ -16,76 +19,7 @@ export default function Home() {
                   Linhas de Pesquisa
                 </h2>
                 <div className="text-gray-600 text-base py-2 overflow-auto touch-pan-y h-80 scroll">
-                  <p className="text-gray-600 text-base">
-                    <h1 className="text-gray-800 text-lg">
-                      {" "}
-                      Linha de pesquisa: Pedagogia da Infância, Políticas e
-                      Diferenças:{" "}
-                    </h1>
-                    <br></br>
-                    <ul>
-                      * Janice Crestani. Participação das famílias no cotidiano
-                      de uma creche: percepções das mães e das professoras de um
-                      CEIM, em Lages, SC. 2021. Dissertação (Mestrado em
-                      Educação) - Universidade do Planalto Catarinense,
-                      Prefeitura Municipal de Lages. Orientador: Jaime Farias
-                      Dresch. <a href="Link"> [Link]</a>
-                    </ul>
-
-                    <ul>
-                      * Karine Tatiane Silva Bottega. Pedagogia para Pequena
-                      Infância: diálogos sobre docência com professoras de bebês
-                      e crianças bem pequenas. 2021. Dissertação (Mestrado em
-                      Educação) - Universidade do Planalto Catarinense,
-                      Prefeitura Municipal de Lages. Orientador: Jaime Farias
-                      Dresch.
-                      <a href="Link"> [Link]</a>
-                    </ul>
-                    <ul>
-                      * Viviane de Lourdes Costa Rosa dos Santos. A valorização
-                      do magistério como discurso: um estudo das políticas
-                      educacionais brasileiras contemporâneas. 2020. Dissertação
-                      (Mestrado em Educação) - Universidade do Planalto
-                      Catarinense, Prefeitura Municipal de Lages. Orientador:
-                      Jaime Farias Dresch.
-                      <a href="https://data.uniplaclages.edu.br/mestrado_educacao/dissertacoes/254b6dc3f0617e72e9231ce5468bc29b.pdf">
-                        [Link]
-                      </a>
-                      <ul>
-                        * Lucimar Presotto Azi Costa. O direito de acesso à
-                        educação infantil e a oferta de vagas na rede municipal
-                        de Lages/SC. 2020. Dissertação (Mestrado em Educação) -
-                        Universidade do Planalto Catarinense, Prefeitura
-                        Municipal de Lages. Orientador: Jaime Farias Dresch.
-                        [Link]
-                      </ul>
-                      <ul>
-                        {" "}
-                        * Isabel Nercolini Ceron. Vestígios da música na
-                        história da Educação Infantil do município de Lages/SC.
-                        2020. Dissertação (Mestrado em Educação) - Universidade
-                        do Planalto Catarinense, Prefeitura Municipal de Lages.
-                        Orientador: Jaime Farias Dresch. [Link]
-                      </ul>
-                      <ul>
-                        {" "}
-                        * Daniela Oliveira de Sá Toigo. Profissionalização da
-                        docência na Educação Infantil na rede municipal de
-                        Lages-SC. 2020. Dissertação (Mestrado em Educação) -
-                        Universidade do Planalto Catarinense, Prefeitura
-                        Municipal de Lages. Orientador: Jaime Farias Dresch.
-                        [Link]
-                      </ul>
-                      <ul>
-                        {" "}
-                        *Luciana Nunes Garcia Ferreira. A literatura no processo
-                        de aprendizagem na educação infantil. 2019. Dissertação
-                        (Mestrado em Educação) - Universidade do Planalto
-                        Catarinense, Prefeitura Municipal de Lages.
-                        Coorientador: Jaime Farias Dresch. [Link]
-                      </ul>
-                    </ul>
-                  </p>
+                  {HtmlParser(props.content[0].description_linhas)}
                 </div>
               </div>
             </div>
@@ -98,76 +32,7 @@ export default function Home() {
                   Pesquisas em andamento
                 </h2>
                 <div className="text-gray-600 text-base py-2 overflow-auto touch-pan-y h-80 scroll">
-                  <p className="text-gray-600 text-base">
-                    <h1 className="text-gray-800 text-lg">
-                      {" "}
-                      Linha de pesquisa: Pedagogia da Infância, Políticas e
-                      Diferenças:{" "}
-                    </h1>
-                    <br></br>
-                    <ul>
-                      * Janice Crestani. Participação das famílias no cotidiano
-                      de uma creche: percepções das mães e das professoras de um
-                      CEIM, em Lages, SC. 2021. Dissertação (Mestrado em
-                      Educação) - Universidade do Planalto Catarinense,
-                      Prefeitura Municipal de Lages. Orientador: Jaime Farias
-                      Dresch. <a href="Link"> [Link]</a>
-                    </ul>
-
-                    <ul>
-                      * Karine Tatiane Silva Bottega. Pedagogia para Pequena
-                      Infância: diálogos sobre docência com professoras de bebês
-                      e crianças bem pequenas. 2021. Dissertação (Mestrado em
-                      Educação) - Universidade do Planalto Catarinense,
-                      Prefeitura Municipal de Lages. Orientador: Jaime Farias
-                      Dresch.
-                      <a href="Link"> [Link]</a>
-                    </ul>
-                    <ul>
-                      * Viviane de Lourdes Costa Rosa dos Santos. A valorização
-                      do magistério como discurso: um estudo das políticas
-                      educacionais brasileiras contemporâneas. 2020. Dissertação
-                      (Mestrado em Educação) - Universidade do Planalto
-                      Catarinense, Prefeitura Municipal de Lages. Orientador:
-                      Jaime Farias Dresch.
-                      <a href="https://data.uniplaclages.edu.br/mestrado_educacao/dissertacoes/254b6dc3f0617e72e9231ce5468bc29b.pdf">
-                        [Link]
-                      </a>
-                      <ul>
-                        * Lucimar Presotto Azi Costa. O direito de acesso à
-                        educação infantil e a oferta de vagas na rede municipal
-                        de Lages/SC. 2020. Dissertação (Mestrado em Educação) -
-                        Universidade do Planalto Catarinense, Prefeitura
-                        Municipal de Lages. Orientador: Jaime Farias Dresch.
-                        [Link]
-                      </ul>
-                      <ul>
-                        {" "}
-                        * Isabel Nercolini Ceron. Vestígios da música na
-                        história da Educação Infantil do município de Lages/SC.
-                        2020. Dissertação (Mestrado em Educação) - Universidade
-                        do Planalto Catarinense, Prefeitura Municipal de Lages.
-                        Orientador: Jaime Farias Dresch. [Link]
-                      </ul>
-                      <ul>
-                        {" "}
-                        * Daniela Oliveira de Sá Toigo. Profissionalização da
-                        docência na Educação Infantil na rede municipal de
-                        Lages-SC. 2020. Dissertação (Mestrado em Educação) -
-                        Universidade do Planalto Catarinense, Prefeitura
-                        Municipal de Lages. Orientador: Jaime Farias Dresch.
-                        [Link]
-                      </ul>
-                      <ul>
-                        {" "}
-                        *Luciana Nunes Garcia Ferreira. A literatura no processo
-                        de aprendizagem na educação infantil. 2019. Dissertação
-                        (Mestrado em Educação) - Universidade do Planalto
-                        Catarinense, Prefeitura Municipal de Lages.
-                        Coorientador: Jaime Farias Dresch. [Link]
-                      </ul>
-                    </ul>
-                  </p>
+                  {HtmlParser(props.content[0].description_andamento)}
                 </div>
               </div>
             </div>
@@ -179,45 +44,7 @@ export default function Home() {
                   Pesquisas concluídas
                 </h2>
                 <div className="text-gray-600 text-base py-2 overflow-auto touch-pan-y h-80 scroll">
-                  <p className="text-gray-600 text-base">
-                    <h1 className="text-gray-800 text-lg">
-                      {" "}
-                      Linha de pesquisa: Pedagogia da Infância, Políticas e
-                      Diferenças:{" "}
-                    </h1>
-                    <br></br>
-                    <ul>
-                      * Andréia Luciane Borges. Inclusão na educação infantil:
-                      crianças com deficiência intelectual. Início: 2020.
-                      Dissertação (Mestrado em Educação) - Universidade do
-                      Planalto Catarinense. (Orientador: Jaime Farias Dresch).
-                    </ul>
-                    <ul>
-                      * Joice Kelly de Oliveira Macedo Silva. O planejamento
-                      escolar e as práticas pedagógicas com foco nas datas
-                      comemorativas. Início: 2021. Dissertação (Mestrado em
-                      Educação) - Universidade do Planalto Catarinense.
-                      (Orientador: Jaime Farias Dresch).
-                    </ul>
-                    * Marcele Antunes de Oliveira. A importância da afetividade
-                    na Educação Infantil. Início: 2021. Dissertação (Mestrado em
-                    Educação) - Universidade do Planalto Catarinense.
-                    (Orientador: Jaime Farias Dresch).
-                    <ul>
-                      * Marcia Aparecida Oliveira. Educação Infantil na
-                      atualidade enfrentando as convergências e consequências de
-                      mães adolescentes e a participação da família na escola.
-                      Início: 2020. Dissertação (Mestrado em Educação) -
-                      Universidade do Planalto Catarinense. (Orientador: Jaime
-                      Farias Dresch).
-                    </ul>
-                    <ul>
-                      Sandra Mara Fernandes. Período de adaptação à Educação
-                      Infantil. Início: 2021. Dissertação (Mestrado em Educação)
-                      - Universidade do Planalto Catarinense. (Orientador: Jaime
-                      Farias Dresch).
-                    </ul>
-                  </p>
+                  {HtmlParser(props.content[0].description_concluidas)}
                 </div>
               </div>
             </div>
@@ -226,4 +53,10 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+export async function getServerSideProps(){
+  const response = await axios.get(process.env.BACKEND + "pesquisa");
+  
+  return {props: {content: response.data}};
 }
