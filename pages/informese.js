dd722aa69975a4050b84e61ef10f22cc3ce435e5
Uniplac <github@uniplaclages.edu.br>
2022-07-28 10:36:24 -0300

    populando

--- pages/informese.js
+++ pages/informese.js
@@ -5,16 +5,16 @@ export default function Home(props) {
   const conteudo = props.content.informs[0]
   return (
     <>
-      <div className="sm:mx-20 md:mx-30 lg:mx-48 min-h-screen mb-12 pt-12">
+      <div className="mx-4 sm:mx-20 md:mx-30 lg:mx-36 xl:mx-48 min-h-screen mb-12 pt-12">
         <div className="textoBasico">
           <div className="titulo-principal">
             <h1>Informe-se</h1>
           </div>
 
           <section id="cards-about">
-            <div className=" px-2 gap-1 place-content-around">
-              <div className="w-full px-1 py-3 flex items-center">
-                <div className="w-full  rounded overflow-hidden shadow-lg bg-card-text py-6 px-4 bg-card">
+            <div className=" gap-1 place-content-around">
+              <div className="w-full py-3 flex items-center">
+                <div className="w-full rounded overflow-hidden shadow-lg bg-card-text py-4 px-4 bg-card">
                   <h2 className="text-2xl text-gray-700 text-center py-1 font-bold">
                     Sobre nós
                   </h2>
@@ -24,8 +24,8 @@ export default function Home(props) {
                 </div>
               </div>
 
-              <div className="w-full px-1 py-3 flex items-center">
-                <div className=" w-full  rounded overflow-hidden shadow-lg bg-card-text py-6 px-4 bg-card">
+              <div className="w-full py-3 flex items-center">
+                <div className=" w-full rounded overflow-hidden shadow-lg bg-card-text py-4 px-4 bg-card">
                   <h2 className="text-2xl text-gray-700 text-center py-1 font-bold ">
                     Objetivos específicos
                   </h2>
@@ -35,8 +35,8 @@ export default function Home(props) {
                 </div>
               </div>
 
-              <div className="w-full px-1 py-3 flex items-center">
-                <div className="w-full  rounded overflow-hidden shadow-lg bg-card-text py-6 px-4 bg-card">
+              <div className="w-full py-3 flex items-center">
+                <div className="w-full rounded overflow-hidden shadow-lg bg-card-text py-4 px-4 bg-card">
                   <h2 className="text-2xl text-gray-700 text-center py-1 font-bold">
                     Projetos
                   </h2>
@@ -46,8 +46,8 @@ export default function Home(props) {
                 </div>
               </div>
 
-              <div className="w-full px-1 py-3 flex items-center">
-                <div className="w-full rounded overflow-hidden shadow-lg bg-card-text py-6 px-4 bg-card">
+              <div className="w-full py-3 flex items-center">
+                <div className="w-full rounded overflow-hidden shadow-lg bg-card-text py-4 px-4 bg-card">
                   <h2 className="text-2xl text-gray-700 text-center py-1 font-bold">
                     Produções
                   </h2>
